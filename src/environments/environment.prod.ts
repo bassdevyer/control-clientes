334d8c85fe668439be5073529a657945d7471218
Miguel Torres <miguetv17@gmail.com>
2020-06-02 18:00:52 -0500

    FirestoreModules config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firestore: {
+    apiKey: "AIzaSyCzHO5daP_zG-Z7Gb4_I7M0xvxaqrsPyp8",
+    authDomain: "control-clientes-3281f.firebaseapp.com",
+    databaseURL: "https://control-clientes-3281f.firebaseio.com",
+    projectId: "control-clientes-3281f",
+    storageBucket: "control-clientes-3281f.appspot.com",
+    messagingSenderId: "931847319521",
+    appId: "1:931847319521:web:17f5343c6513d2e3ccaca0"
+  }
 };
