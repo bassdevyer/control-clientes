a0684e605752171ee534fe24d891993bf7f3c110
Miguel Torres <miguetv17@gmail.com>
2020-08-08 10:29:27 -0500

    Componente de registro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,8 @@ import {ConfiguracionComponent} from './componentes/configuracion/configuracion.
 import {NoEncontradoComponent} from './componentes/no-encontrado/no-encontrado.component';
 import {PiePaginaComponent} from './componentes/pie-pagina/pie-pagina.component';
 import {ClienteServicio} from "./servicios/cliente.service";
+import {LoginService} from "./servicios/login.service";
+import {AuthGuard} from "./guardianes/aith.guard";
 
 @NgModule({
   declarations: [
@@ -42,7 +44,7 @@ import {ClienteServicio} from "./servicios/cliente.service";
     FormsModule,
     FlashMessagesModule.forRoot(),
   ],
-  providers: [ClienteServicio],
+  providers: [ClienteServicio, LoginService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/componentes/registro/registro.component.ts
+++ src/app/componentes/registro/registro.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FlashMessagesService} from "angular2-flash-messages";
+import {LoginService} from "../../servicios/login.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-registro',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
+  email: string;
+  password: string;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private flashMessages: FlashMessagesService,
+    private loginService: LoginService
+  ) {
+  }
 
   ngOnInit(): void {
+    this.loginService.getAuth().subscribe(auth => {
+      if (auth) {
+        this.router.navigate(['/'])
+      }
+    })
+  }
+
+  registro() {
+    this.loginService.registrarse(this.email, this.password)
+      .then(res => {
+        this.router.navigate(['/'])
+
+      })
+      .catch(error => {
+          this.flashMessages.show(error.message, {
+            cssClass: 'alert-danger', timeout: 4000
+          })
+        }
+      )
   }
 
 }
